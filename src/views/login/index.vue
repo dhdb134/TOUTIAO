d53d2dca18c8a50d863cc90b114db118038e9734
xxx <343629093@qq.com>
2022-09-02 07:35:19 +0800

    9.2 moring

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,13 +7,15 @@
       v-model="user.mobile"
       name="手机号"
       placeholder="请输入手机号"
-      :rules="[{ required: true, message: '请填写用户名' }]"
+      :rules="userFormrules.mobile"
+      type="number"
     ><i slot="left-icon" class="iconfont icon-shouji"></i></van-field>
     <van-field
       v-model="user.code"
       name="验证码"
       placeholder="请输入验证码"
-      :rules="[{ required: true, message: '请填写密码' }]"
+      :rules="userFormrules.code"
+      type="number"
     ><i slot="left-icon" class="iconfont icon-yanzhengma"></i>
     <template #button>
     <van-button class="send-sms-btn" round size="small" type="primary">发送验证码</van-button>
@@ -37,17 +39,47 @@ export default {
       user: {
         mobile: '',
         code: ''
+      },
+      userFormrules: {
+        mobile: [
+          {
+            required: true,
+            message: '手机不能为空'
+          },
+          {
+            pattern: /^1[3456789]\d{9}$/,
+            message: '手机号码错误'
+          }],
+        code: [{
+          required: true,
+          message: '验证码不能为空'
+        },
+        {
+          pattern: /^\d{4}$/,
+          message: '验证码错误'
+        }
+        ]
       }
     }
   },
   methods: {
     async  onSubmit () {
       const user = this.user
+      this.$toast.loading({
+        message: '加载中...',
+        forbidClick: true,
+        duration: 0
+      })
       try {
         const res = await login(user)
         console.log('登录成功', res)
+        this.$toast.success('登录成功')
       } catch (err) {
-        console.log('登录失败', err)
+        if (err.response.status === 400) {
+          this.$toast.success('手机号或验证码错误')
+        } else {
+          this.$toast.success('登录失败')
+        }
       }
     }
   }
