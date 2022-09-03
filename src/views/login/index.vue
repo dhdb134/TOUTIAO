95ffc37d25866704116ac6c5b39b4b5e45447345
xxx <343629093@qq.com>
2022-09-03 12:54:09 +0800

    9.3 12.54

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -85,8 +85,8 @@ export default {
         duration: 0
       })
       try {
-        const res = await login(user)
-        console.log('登录成功', res)
+        const { data } = await login(user)
+        this.$store.commit('setUser', data.data)
         this.$toast.success('登录成功')
       } catch (err) {
         if (err.response.status === 400) {
